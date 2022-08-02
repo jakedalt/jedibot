bc023ec078fd0c1d64f4b9e509ed7145c2a76797
JKAKE\KingV5 <jakedalt128@gmail.com>
2022-08-02 11:37:53 -0500

    rollback

--- db.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='bg3worlddomination123#',
-                              host='127.0.0.1',
-                              database='jedibot')
-cnx.close()
