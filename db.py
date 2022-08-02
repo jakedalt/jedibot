3b3a0bd8d1524e8e7c3273d130a89e55b74c28ab
Jake Dalton <43936976+jakedalt@users.noreply.github.com>
2022-08-02 11:23:46 -0500

    init commit

---
+++ db.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='bg3worlddomination123#',
+                              host='127.0.0.1',
+                              database='jedibot')
+cnx.close()
---
+++ utils.py
@@ -0,0 +1,1 @@
+def createIVC(guild):
