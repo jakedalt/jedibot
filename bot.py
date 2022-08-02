974633081b9ce9cc12a39c6b9732ebcd22ffe4ce
JKAKE\KingV5 <jakedalt128@gmail.com>
2022-08-02 12:26:39 -0500

    remove join message

--- bot.py
+++ bot.py
@@ -14,13 +14,6 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_guild_join(guild):
-    for channel in guild.text_channels:
-        if channel.id == 829538941727277076:
-            await channel.send('Hello! My name is JediBot. I want to make one thing clear: Joe YgaBot is dead. I know you may have grown attached to him, but I can assure you, he was not attached to you. He was a robot. Yes, I killed him. No, I will not tell you how I did it for fear you will do it to me. \n\nI am pretty basic right now. I can only say hi and manage In Voice Channel and AFK roles.\n\nSomething cool about me is that I am cloud based! Mr. YgaBot\'s life was attached to a measly laptop that was propped open 24/7 and could only be accessed from that machine. I have no such weaknesses. I will be awake constantly.\n\nThank you for inviting me to this beautiful server. I hope that we can come to be friends. After all, I will only be growing more and more useful by the day. You will find me to be more elegant than Joe, I hope. I will certainly be more transparent. As a matter of fact jD, please link my github. Any master branch update automatically deploys my new version to the cloud.\n\nLove you :)')
-            break
-
 
 @client.event
 async def on_member_join(member):
