6f6fa3ed688c196d15c470da8b1ecc40f9c4d54d
JKAKE\KingV5 <jakedalt128@gmail.com>
2022-08-04 15:05:05 -0500

    update help

--- src/onMessage.py
+++ src/onMessage.py
@@ -17,15 +17,15 @@ def messageResponse(discord, client, content, author):
     if content.lower().strip() == 'jedi':
         return 'What do you want? Try `jedi help` maybe.'
 
-    if content.lower().strip() == 'jedi help':
+    if content.lower().strip() == 'jedi help' or content.lower().strip() == 'jedihelp':
         embed = discord.Embed(title='JediBot Help', description='Oh my! You are in need of assistance? Try using one '
                                                                 'of my numerous commands\n\nUsage: `jedi '
                                                                 '<command>` or `@JediBot <command>`',
                               color=discord.Color.gold())
-        embed.add_field(name='Commands:', value='`help`\n`stats`')
+        embed.add_field(name='Commands:', value='`help`\n`stats`\n\n`kanye`\n`checkme`')
         return embed
 
-    if content.lower().strip() == 'jedi stats':
+    if content.lower().strip() == 'jedi stats' or content.lower().strip() == 'jedistats':
         stats = getUserStats(author.id)
         if stats is None:
             print('stat error, getUserStats returned None: ' + str(author.name) + ' id: ' + str(author.id))
