b05abb65fecdb1c63c5148db4ef0f59f134b6c14
jrkohout <jrkohout64@gmail.com>
2022-08-07 22:53:48 -0500

    add embedded poll feature

--- src/commands.py
+++ src/commands.py
@@ -105,6 +105,45 @@ def register_commands(bot):
         response = await geturl(' '.join((search_term, *other_search_terms)))
         await ctx.send(response)
 
+    @bot.command()
+    async def poll(ctx, title, option1, option2, *other_options):
+        """Create a poll to gauge opinion
+
+        :param ctx: calling context
+        :param title: title of poll
+        :param option1: first option for users to pick from
+        :param option2: second option for users to pick from
+        :param other_options: remaining options for users to pick from
+        """
+        # TODO could integrate further by using on_reaction_add and on_reaction_remove events
+        if len(other_options) > 18:
+            await ctx.send("This feature only supports up to 20 options.")
+            return
+
+        embed = discord.Embed(
+            title=f"Poll: {title}",
+            type='rich',
+            color=discord.Color.blue()
+        )
+        embed.add_field(name='*Choose between the following options:*', value='\u200b')
+        unicode_emojis = [u'\U0001F1E6', u'\U0001F1E7', u'\U0001F1E8', u'\U0001F1E9', u'\U0001F1EA', u'\U0001F1EB',
+                          u'\U0001F1EC', u'\U0001F1ED', u'\U0001F1EE', u'\U0001F1EF', u'\U0001F1F0', u'\U0001F1F1',
+                          u'\U0001F1F2', u'\U0001F1F3', u'\U0001F1F4', u'\U0001F1F5', u'\U0001F1F6', u'\U0001F1F7',
+                          u'\U0001F1F8']
+
+        max_idx = 0
+        for idx, opt in enumerate((option1, option2, *other_options)):
+            embed.add_field(
+                name=f'{unicode_emojis[idx]}\t{opt}',
+                value='\u200b',
+                inline=False
+            )
+            max_idx += 1
+        embed.set_footer(text="Vote by clicking on one of the reactions below.")
+        message = await ctx.send(embed=embed)
+        for i in range(max_idx):
+            await message.add_reaction(unicode_emojis[i])
+
     async def on_command_error(ctx, error):
         # TODO see https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#exceptions for more exceptions
         if isinstance(error, commands.MissingRequiredArgument):
